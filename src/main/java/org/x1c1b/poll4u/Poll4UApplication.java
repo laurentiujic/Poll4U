b5e61220550896876e11b344b9e4a8fcda3457ce
0x1C1B <32311121+0x1C1B@users.noreply.github.com>
2019-06-28 14:48:38 +0200

    Setup minimal project configuration with Spring IoC support

---
+++ src/main/java/org/x1c1b/poll4u/Poll4UApplication.java
@@ -0,0 +1,13 @@
+package org.x1c1b.poll4u;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Poll4UApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Poll4UApplication.class, args);
+	}
+
+}
