09db1745583e84f23377bac67f71b886034c9826
0x1C1B <32311121+0x1C1B@users.noreply.github.com>
2019-07-01 09:07:06 +0200

    Configure spring security context and the filter chain

--- src/main/java/org/x1c1b/poll4u/config/SecurityConfig.java
+++ src/main/java/org/x1c1b/poll4u/config/SecurityConfig.java
@@ -1,13 +1,24 @@
 package org.x1c1b.poll4u.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.x1c1b.poll4u.security.UserPrincipalService;
+import org.x1c1b.poll4u.web.TokenAuthenticationEntryPoint;
+import org.x1c1b.poll4u.web.filter.TokenAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -16,7 +27,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
         jsr250Enabled = true,
         prePostEnabled = true
 )
+@ComponentScan("org.x1c1b.poll4u.security")
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired UserPrincipalService principalService;
+    @Autowired private TokenAuthenticationEntryPoint unauthorizedHandler;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -24,11 +39,57 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public TokenAuthenticationFilter tokenAuthenticationFilter() {
+
+        return new TokenAuthenticationFilter();
+    }
+
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+
+        authenticationManagerBuilder
+                .userDetailsService(principalService)
+                .passwordEncoder(passwordEncoder());
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
         http.cors().and()
-                .csrf().disable()
-                .authorizeRequests().anyRequest().permitAll();
+                .csrf()
+                    .disable()
+                .exceptionHandling()
+                    .authenticationEntryPoint(unauthorizedHandler)
+                    .and()
+                .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    .and()
+                .authorizeRequests()
+                    .antMatchers("/",
+                        "/favicon.ico",
+                        "/**/*.png",
+                        "/**/*.gif",
+                        "/**/*.svg",
+                        "/**/*.jpg",
+                        "/**/*.html",
+                        "/**/*.css",
+                        "/**/*.js")
+                        .permitAll()
+                    .antMatchers(HttpMethod.POST,"/auth", "/users")
+                        .permitAll()
+                    .antMatchers(HttpMethod.GET, "/users/**")
+                        .permitAll()
+                    .anyRequest()
+                        .authenticated();
+
+        http.addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 }
---
+++ src/main/java/org/x1c1b/poll4u/web/filter/TokenAuthenticationFilter.java
@@ -0,0 +1,65 @@
+package org.x1c1b.poll4u.web.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.x1c1b.poll4u.security.TokenProvider;
+import org.x1c1b.poll4u.security.UserPrincipalService;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class TokenAuthenticationFilter extends OncePerRequestFilter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TokenAuthenticationFilter.class);
+
+    @Autowired private TokenProvider<Long> tokenProvider;
+    @Autowired private UserPrincipalService principalService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+
+        try {
+
+            String token = extractToken(httpServletRequest);
+
+            if (StringUtils.hasText(token) && tokenProvider.validate(token)) {
+
+                Long userId = tokenProvider.getPayloadFromToken(token);
+
+                UserDetails userDetails = principalService.loadUserById(userId);
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpServletRequest));
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        } catch (Exception exc) {
+
+            LOGGER.error("Could not set user authentication in security context", exc);
+        }
+
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
+
+    private String extractToken(HttpServletRequest request) {
+
+        String header = request.getHeader("Authorization");
+
+        if (StringUtils.hasText(header) && header.startsWith("Bearer ")) {
+
+            return header.substring(7);
+        }
+
+        LOGGER.error("Could not extract token, token is missing/malformed in request");
+        return null;
+    }
+}
