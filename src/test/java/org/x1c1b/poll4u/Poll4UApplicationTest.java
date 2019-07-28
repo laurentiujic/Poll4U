e8b85e0250b86bbdb903d3717e1b49d0efe58a17
Laurentiu Jicmon <52014270+laurentiujic@users.noreply.github.com>
2019-07-28 19:45:03 +0200

    Update Poll4UApplicationTest.java
    contextLoad test now working

--- src/test/java/org/x1c1b/poll4u/Poll4UApplicationTest.java
+++ src/test/java/org/x1c1b/poll4u/Poll4UApplicationTest.java
@@ -6,9 +6,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@ContextConfiguration
 @SpringBootTest
 public class Poll4UApplicationTest {
-
+	static {
+		System.setProperty("poll4u.security.secret", "laur");
+	}
 	@Test
 	public void contextLoads() {
 	}
